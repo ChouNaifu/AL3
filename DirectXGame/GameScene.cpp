b40c3cee5442b01138380810a73a916eae0c662d
ChouNaifu <k024g1023@g.neec.ac.jp>
2025-04-14 15:08:33 +0900

    4/14

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -5,6 +5,7 @@ GameScene::GameScene() {}
 
 GameScene::~GameScene() { 
 	delete sprite_; 
+	sprite_ = nullptr;
 }
 
 void GameScene::Initialize() { 
@@ -12,7 +13,15 @@ void GameScene::Initialize() {
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	Vector2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	sprite_->SetPosition(position);
+	//sprite_->SetScale({2.0f, 2.0f});
+	//sprite_->SetRotation(45.0f);
+	//sprite_->SetColor({1.0f, 1.0f, 1.0f, 1.0f});
+}
 
 void GameScene::Draw() {
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
